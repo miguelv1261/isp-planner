a56327e8c7f72b88b06e918e193e177690b86b26
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2026-03-23 15:58:41 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
-import './index.css' // opcional
+import './index.css'
+
+import { registerSW } from 'virtual:pwa-register'
+
+// 🔥 registrar service worker ANTES o justo en el flujo principal
+registerSW({
+  immediate: true
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW()
